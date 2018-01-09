86dcdb3f410064decd53232f62257fd88fedfb10
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-09 15:36:06 -0800

    have a search bar on the page

--- lab-shannon/src/main.js
+++ lab-shannon/src/main.js
@@ -14,7 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <button>Testing</button>
+        <SearchForm />
       </div>
     );
   }
--- lab-shannon/src/search-form.js
+++ lab-shannon/src/search-form.js
@@ -3,5 +3,28 @@ import React from 'react';
 class searchForm extends React.Component {
   constructor(props){
     super(props);
+
+    this.state = {filter: ''};
+    this.filterInput = this.filterInput.bind(this);
+    this.runSearch = this.runSearch.bind(this);
+  }
+
+  filterInput(event) {
+    this.setState({filter: event.target.value});
+  }
+
+  runSearch() {
+    this.setState();
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.filter} onChange={this.filterInput} />
+        <button onClick={this.runSearch}>Search</button>
+      </div>
+    );
   }
 }
+
+module.exports = searchForm;
