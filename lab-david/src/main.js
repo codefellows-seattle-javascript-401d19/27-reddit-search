798c15395fbc0cd7b3c1a321c8d75f1d09df9fc7
David Lindahl <davidalindahl@gmail.com>
2018-01-09 13:39:53 -0800

    setup of main js file

---
+++ lab-david/src/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import './style/main.scss';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+ReactDom.render(<App/>, document.getElementById('content'));
