22735c126bd946e0d01926cf2f3a19cd3f067656
Jacob M-G Evans <jacobmgevans@gmail.com>
2018-01-09 16:07:18 -0800

    scaffolding

---
+++ lab-jacob/src/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style/main.scss';
+import searchForm from './component/search-form';
+import searchResultList from './component/search-result-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+
+    };
+  }
+ 
+  
+}
