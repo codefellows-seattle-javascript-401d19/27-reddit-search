801b2f8259db851664e6c0dec4a086d1c4b500b0
Dalton Carr <daltonlcarr@yahoo.com>
2018-01-09 17:17:02 -0800

    able to type into input

--- src/components/wizard-school-form.js
+++ src/components/wizard-school-form.js
@@ -4,6 +4,21 @@ import React from 'react';
 class WizardForm extends React.Component {
     constructor(props){
         super(props)
+
+
+        this.handleInputChange = this.handleInputChange.bind(this)
+
+        this.state = {
+            name: ''
+        }
+
+        
+    }
+
+    handleInputChange() {
+        console.log('changing')
+
+        this.setState({name: event.target.value})
     }
 
     render() {
@@ -12,9 +27,11 @@ class WizardForm extends React.Component {
 
                 <div>
                     Name:
-                    <input type="text" value="" placeholder="YOYO"/>
+                    <input type="text" value={this.state.name} placeholder="YOYO" onChange={this.handleInputChange}/>
                 </div>
-
+                <div>
+                    <input type="submit"/>
+                </div>
             </form>
         )
     }
