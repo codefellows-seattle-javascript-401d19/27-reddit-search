6f069cf20f134cd1cfdd667073ebecbd764ac442
jjkusowski <jjkusowski@yahoo.com>
2018-01-09 20:43:56 -0800

    all works.  Red error border added.  1-100 limit added.

--- lab-jeff/README.md
+++ lab-jeff/README.md
@@ -1,7 +1,7 @@
 # Codefellows javascript 401 lab 26 - REACT cowsay demo
 
 ## Purpose
-Learn to use react, webpack, and related technologies by rendering a cow with ipsum text using cowsay and faker.
+Learn to use react, webpack, and related technologies by creating an API which searches the subreddits on reddit.com for an entered search term.  If the search term is found, a list of the top topics will be returned.  The amount returned can be set by entering a limit in the second input box.  If none is entered, reddit defaults to 25.
 
 ## To use
 Install dependencies:
@@ -10,11 +10,11 @@ Run webpack dev server:
 ```npm run watch```
 Open your browser at localhost:8080.
 
-Click the "click me" button to your heart's content.
+Enter a topic to search on reddit in the first input field.  Enter the number of items you wish to have returned between 1 and 100.  If none is entered, reddit will default to 25.  If no subreddit is found with the given search term, the input form will get a red border and a message will be displayed.  
 
 ## Dependencies
 React - https://reactjs.org
 Babel - https://babeljs.io
 Webpack - https://webpack.js.org
-Faker - https://www.npmjs.com/package/Faker
+Superagent - https://visionmedia.github.io/superagent/
 
--- lab-jeff/src/components/search-form.js
+++ lab-jeff/src/components/search-form.js
@@ -6,7 +6,8 @@ class SearchForm extends React.Component {
     super(props);
     this.state = {
       searchBoardForm: '',
-      searchFormLimit: 100,
+      searchFormLimit: undefined,
+      searchError: false,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleTopic = this.handleTopic.bind(this);
@@ -22,24 +23,27 @@ class SearchForm extends React.Component {
   }
   handleSubmit(event){
     event.preventDefault();
-    console.log('clicked');
     console.log('searchboardform: ' + this.state.searchBoardForm);
     console.log('searchformlimit: ' + this.state.searchFormLimit);
     superagent.get(`http://www.reddit.com/r/${this.state.searchBoardForm}.json?limit=${this.state.searchFormLimit}`)
       .then(results => {
         console.log(results);
+        this.setState({searchError: false});
         this.props.updateTopics(results.body.data.children);
       })
       .catch(error =>{
+        this.props.updateTopics([]);
+        this.setState({searchError: true});
         console.log(error);
       });
   }
 
   render() {
-    return <form onSubmit={this.handleSubmit}>
+    return <form className={this.state.searchError === true ? 'error' : 'normal'} onSubmit={this.handleSubmit}>
       <input type="text" name="topic" value={this.state.topic} onChange={this.handleTopic} placeholder="search term here"/>
-      <input type="number" name="limit" value={this.state.limit} onChange={this.handleLimit} placeholder="how many items" />
+      <input type="number" name="limit" value={this.state.limit} onChange={this.handleLimit} placeholder="how many items" min='0' max='100'/>
       <button type="submit">Search</button>
+      <p className={this.state.searchError === true ? 'show' : 'no-show'}> Search topic not found</p>
     </form>;
   }
 
