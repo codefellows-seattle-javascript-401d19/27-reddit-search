0b0ca819926f43c6bd4d6df7ee267459bd7bf86a
jjkusowski <jjkusowski@yahoo.com>
2018-01-10 08:47:29 -0800

    added upvotes

--- lab-jeff/src/components/search-result-list.js
+++ lab-jeff/src/components/search-result-list.js
@@ -4,7 +4,7 @@ class SearchResultList extends React.Component {
   displayTopics() {
     return this.props.topics.map((topic, index) => {
       return <li className="topics" key={index}>
-        <a href={topic.data.url}>{topic.data.title} </a>
+        <a href={topic.data.url}>{topic.data.title} <p>Upvotes: {topic.data.ups}</p></a>
       </li>;
     });
   }
