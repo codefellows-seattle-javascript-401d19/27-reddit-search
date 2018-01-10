a3f2b5330c2c8653bf39e392c6a15a5ae2186223
jjkusowski <jjkusowski@yahoo.com>
2018-01-09 17:39:11 -0800

    limit added

--- lab-jeff/src/components/search-form.js
+++ lab-jeff/src/components/search-form.js
@@ -10,17 +10,21 @@ class SearchForm extends React.Component {
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleTopic = this.handleTopic.bind(this);
+    this.handleLimit = this.handleLimit.bind(this);
 
   }
 
   handleTopic(event){
     this.setState({searchBoardForm: event.target.value})
   }
-  
+  handleLimit(event){
+    this.setState({searchFormLimit: event.target.value})
+  }
   handleSubmit(event){
     event.preventDefault();
     console.log('clicked');
     console.log('searchboardform: ' + this.state.searchBoardForm);
+    console.log('searchformlimit: ' + this.state.searchFormLimit);
     superagent.get(`http://www.reddit.com/r/${this.state.searchBoardForm}.json?limit=${this.state.searchFormLimit}`)
     .then(results => {
       console.log(results);
@@ -33,7 +37,8 @@ class SearchForm extends React.Component {
 
   render() {
     return <form onSubmit={this.handleSubmit}>
-        <input type="text" name="board" value={this.state.topic} onChange={this.handleTopic} placeholder="search term here"/>
+        <input type="text" name="topic" value={this.state.topic} onChange={this.handleTopic} placeholder="search term here"/>
+        <input type="number" name="limit" value={this.state.limit} onChange={this.handleLimit} placeholder="how many items" />
         <button type="submit">Search</button>
       </form>
   }
