86ad442e4e8546f22db2785a63dfb9151ae6a944
Catherine Looper <loopercat@gmail.com>
2018-08-01 13:21:38 -0700

    refactoring code to maintain state in app

--- lab-catherine/src/components/search-form.js
+++ lab-catherine/src/components/search-form.js
@@ -1,54 +1,33 @@
 import React, {Component, Fragment} from 'react';
 import superagent from 'superagent';
 
-export default class SearchForm extends Component {
-  state = {
-    topic: '',
-    limit: '',
-    errorExists: false,
-  };
-
-  handleTopic = ({target: { value }}) => this.setState({topic: value});
-
-  handleLimit = ({target: {value}}) => this.setState({limit: value});
-  
-  handleSubmit = (event) => {
-    const {
-      state: {topic, limit},
-    } = this;
-    event.preventDefault();
-    superagent.get(`http://www.reddit.com/r/${topic}.json?limit=${limit}`)
-      .then(response => {
-        this.setState({errorExists: false});
-        this.props.setResults(response.body.data.children);
-      })
-      .catch(error => {
-        this.props.setResults([]);
-        this.setState({errorExists: true});
-      });
-  }
-
-  render () {
-    const {
-      handleSubmit,
-      handleTopic,
-      handleLimit,
-      state: {errorExists, topic, limit},
-    } = this;
-    return (
-      <Fragment>
-        <form onSubmit={handleSubmit}>
-          <label>Topic:</label>
-          <input className={errorExists ? 'error' : 'normal'} type="text" name='topic' placeholder='enter subreddit' value={topic} onChange={handleTopic}/>  
+const SearchForm = ({handleTopic, handleLimit, handleSubmit, errorExists}) => (
+  <Fragment>
+    <form onSubmit={handleSubmit}>
+      <label>Topic:</label>
+      <input 
+        className={errorExists ? 'error' : 'normal'} 
+        type='text'
+        name='topic' 
+        placeholder='enter subreddit' 
+        onChange={handleTopic}
+      />  
             &nbsp;
 
-          <label>Limit:</label>
-          <input type="number" name='limit' placeholder='#' min='0' max='100' value={limit} onChange={handleLimit}/>      
+      <label>Limit:</label>
+      <input 
+        type="number" 
+        name='limit' 
+        placeholder='#' 
+        min='0' 
+        max='100' 
+        onChange={handleLimit}
+      />      
             &nbsp;
 
-          <button type='submit'>Search</button>
-        </form>
-      </Fragment>
-    );
-  }
-}
+      <button type='submit'>Search</button>
+    </form>
+  </Fragment>
+);
+
+export default SearchForm;
